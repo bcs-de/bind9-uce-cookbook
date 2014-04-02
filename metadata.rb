df388ae23589b844d8bf575498d6b0f214056a57
Arnold Krille <a.krille@b-c-s.de>
2014-04-02 13:29:27 +0200

    Try to fix slave zones.
    And only use zones that are either slave or master.

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
-name             "bind9-chroot"
+name             "bind9-reversezones"
 maintainer       "Tnarik Innael"
+maintainer       "Arnold Krille"
 maintainer_email "tnarik@lecafeautomatique.co.uk"
+maintainer_email "a.krille@b-c-s.de"
 license          "Apache 2.0"
 description      "Installs/Configures bind9 with chroot and hiding CHAOS INFORMATION"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
